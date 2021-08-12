74d4c550da0bb458e1a03751060cc9d49fcb4c31
rayhancse15git <rayhancse15@gmail.com>
2021-08-12 16:18:38 +0600

    Add project files.

---
+++ SwaggerConfigure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SwaggerConfigure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
